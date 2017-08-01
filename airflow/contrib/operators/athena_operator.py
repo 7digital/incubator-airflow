d58dc4f2ffeb9df663a6599d0e2edc4f78c7aae0
Ben Davison <ben.davison@7digital.com>
2017-08-01 16:26:46 +0100

    Add athena operator and hook

---
+++ airflow/contrib/operators/athena_operator.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import logging
+
+from airflow.contrib.hooks.athena_hook import AthenaHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+
+
+class AthenaOperator(BaseOperator):
+    """
+    Executes sql code on Athena
+
+    :param athena_conn_id: reference to a specific Athena instance
+    :type athena_conn_id: string
+    :param sql: the sql code to be executed
+    :type sql: Can receive a str representing a sql statement,
+        a list of str (sql statements), or reference to a template file.
+        Template reference are recognized by str ending in '.sql'
+    :param database: name of database which overwrite defined one in connection
+    :type database: string
+    """
+
+    template_fields = ('sql',)
+    template_ext = ('.sql',)
+    ui_color = '#ededed'
+
+    @apply_defaults
+    def __init__(
+        self, sql, athena_conn_id='athena_default', parameters=None, schema_name=None, *args, **kwargs):
+        super(AthenaOperator, self).__init__(*args, **kwargs)
+        self.athena_conn_id = athena_conn_id
+        self.sql = sql
+        self.parameters = parameters
+        self.schema_name = schema_name
+
+    def execute(self, context):
+        logging.info('Executing: ' + str(self.sql))
+        hook = AthenaHook(athena_conn_id=self.athena_conn_id,
+                          schema=self.database)
+        hook.run(
+            self.sql,
+            parameters=self.parameters)
